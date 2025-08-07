2b7d504ea08d9794786acc883554be6a0cebaf6d
Adeyemi Pelumi <adeyemipelumi1980@gmail.com>
2025-08-07 17:10:47 +0100

    update on the error

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -1,5 +1,4 @@
 import { NextResponse } from "next/server";
-// import { NotionBlog } from "../../../lib/notion";
 import { NotionBlog } from "@/lib/notion";
 // Add these exports for static generation
 export const dynamic = "force-static";
